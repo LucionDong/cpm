007426e623d0b6aff34ffa740b3cae79ba7293e2
dongbin <dongbin0625@163.com>
2025-02-19 17:27:27 +0800

    add-git_info.h-success

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 #include "argparse.h"
 #include "daemon.h"
+#include "./include/git_info.h"
 
 static bool           exit_flag         = false;
 static neu_manager_t *g_manager         = NULL;
@@ -79,6 +80,9 @@ static int neuron_run(const neu_cli_args_t *args)
 
 int main(int argc, char *argv[])
 {
+    printf("git_commit_id: %s\n", GIT_COMMIT_ID);
+    printf("git_tag: %s\n", GIT_TAG);
+
 	int            rv     = 0;
     int            status = 0;
     int            signum = 0;
